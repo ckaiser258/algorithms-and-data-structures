65c370dfe78f28fd59f777d364e74f52ad93cec9
Colton <kaiserc2@seattleu.edu>
2020-10-12 17:18:09 -0700

    Add unrollMatrix practice algo.

---
+++ Practice Algos/unrollMatrix.js
@@ -0,0 +1,25 @@
+let myMatrix = [[1, 2, 3, 4],
+                [12,13,14,5],
+                [11,16,15,6],
+                [10,9, 8, 7]];
+
+function unroll(matrix) {
+  
+  //exit condition
+  
+  
+  // print and delete the top
+  
+  // print and delete the right side
+  
+  
+  // print and delete the bottom
+  
+  // print and delete the left
+  
+  unroll(matrix)
+}
+unroll(myMatrix);
+
+
+// end result: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
