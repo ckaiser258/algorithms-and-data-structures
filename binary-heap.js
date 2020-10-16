f6dfd6ee42a30fb954aaf3693f158a30d0c29f2e
Colton <kaiserc2@seattleu.edu>
2020-10-15 17:32:06 -0700

    ExtractMax() binary heap

--- binary-heap.js
+++ binary-heap.js
@@ -17,4 +17,45 @@ class MaxBinaryHeap {
       index = parentIndex;
     }
   }
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftIndex = 2 * index + 1;
+      let rightIndex = 2 * index + 2;
+      let leftChild, rightChild;
+      let swap = null;
+      //check if left child exists
+      if (leftIndex < length) {
+        leftChild = this.values[leftIndex];
+        if (leftChild > element) {
+          swap = leftIndex;
+        }
+      }
+      //check if right child exists
+      if (rightIndex < length) {
+        rightChild = this.values[rightIndex];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightIndex;
+        }
+      }
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
 }
