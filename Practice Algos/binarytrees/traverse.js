a7a2dec91091d4770a9fb7f0ce8b8dde736e162d
Colton <kaiserc2@seattleu.edu>
2020-10-29 16:39:33 -0700

    Create binary tree folder and add traverse file.

---
+++ Practice Algos/binarytrees/traverse.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ */
+
+ //Preorder traversal checks root and then left and then
+var preorderTraversal = function (root) {
+  let result = [];
+  function traverse(node) {
+    if (node) {
+      // if(node) because we need to make sure an empty array wasn't passed in
+      //Because if an empty array is passed in, .push() would break
+      result.push(node.val);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+  }
+  traverse(root);
+  return result;
+};
